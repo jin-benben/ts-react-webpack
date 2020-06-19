01b087fa684e8a872269c0522aba901cc79e0997
jinwentao <jinwentao@flybycloud.com>
2020-06-19 16:08:50 +0800

    头部

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,15 +1,23 @@
 import React from 'react'
-import { NavLink } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 import  './index.less'
+
+const navList=[
+  {name:'首页',path:'/',id:1},
+  {name:'沸点',path:'/pins',id:2},
+]
 
 const Header=()=>{
   return (
     <div className="main-header-box">
-      <header>
-       
+      <header className="container">
+        <a className="logo">
+          <img src="https://b-gold-cdn.xitu.io/v3/static/img/logo.a7995ad.svg" />
+        </a>
         <nav>
-          <NavLink to='/home'>home</NavLink>
-          <NavLink to='/my'>center</NavLink>
+          {
+            navList.map(item=> <Link className="nav-item" key={item.id} to={item.path}>{item.name}</Link>)
+          }
         </nav>
       </header>
     </div>
